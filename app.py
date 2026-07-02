44df55d745a3d4462d847277fbe5dd538afe9710
sujitx-vs <sujith.vs.cmfeb26@gmail.com>
2026-07-02 10:04:05 +0530

    change in code for docker app.run(host="0.0.0.0"

--- app.py
+++ app.py
@@ -251,4 +251,4 @@ def create_claim():
         flash(f"Claim failed: {exc}", "error")
     return redirect(url_for("index"))
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
